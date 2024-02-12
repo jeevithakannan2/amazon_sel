a4de40fcb2d383e96b12d7e63cec1ee9a27d5aa0
jeevithakannan2 <ksjeevithakannan123@gmail.com>
2024-02-12 10:17:34 +0530

    Working code

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
-import asyncio
-import random
-from time import sleep
+import os
+
 from playwright.async_api import async_playwright
 from playwright_stealth import stealth_async
-
+import uuid
 
 class Amazon:
     def __init__(self):
         self.page = None
-
-    #
-    async def random_delay(self):
-        # Introduce a random delay between 0.5 and 2 seconds
-        return random.uniform(0.5, 2.5)
-
+        self.count = None
+        self.uuidn = str(uuid.uuid4())
+        self.captcha_count = 0
     async def captcha_inp(self, captcha):
         print("Input captcha")
 
         page = self.page
 
-        await page.type("//input[@type='text']", captcha)
+        await page.fill("//input[@type='text']", captcha)
         await page.click('//*[@id="a-autoid-0"]/span/input')
 
-        await page.wait_for_load_state("networkidle0")
         if 'captcha' in await page.content():
             return {"msg": "Wrong captcha"}
         else:
@@ -32,101 +27,143 @@ class Amazon:
         page = self.page
         print("captcha")
         img = page.locator("//img[@alt='captcha']")
+        with open(f'captcha_{self.uuidn}.txt', 'w', ) as f:
+            f.write(str(self.captcha_count))
 
-        await img.screenshot(path='captcha.png')
-        url = page.url.strip()
-        print(url)
-        page.wait_for_timeout(20000)
+        await img.screenshot(path=f'captcha_{self.uuidn}.png')
+
+        await page.wait_for_selector("//*[@id='a-autoid-0']/span/input", state='attached')
+        await page.wait_for_timeout(30000)
+
         if 'captcha' in await page.content():
+            self.captcha_count += 1
             await self.captcha()
         else:
             return
 
     async def run(self, url):
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)
-            context = await browser.new_context()
-            # Apply stealth options to the context
-            self.page = await context.new_page()
-            page = self.page
-            await stealth_async(page)
 
-            # Navigate to a website
-            await page.goto(url)
-            await page.wait_for_load_state("load")
-            await asyncio.sleep(await self.random_delay())
-            await page.click("//input[@id='buy-now-button']")
-
-            await page.type("//input[@type='email']", '9894789409', delay=1.5)
-
-            await page.click("//input[@id='continue']")
-
-            await asyncio.sleep(await self.random_delay())
-            await page.type("//input[@type='password']", 'jeeva2005', delay=1.5)
-
-            await page.click("//input[@id='signInSubmit']")
-            await asyncio.sleep(await self.random_delay())
-
-            await page.wait_for_timeout(10000)
-            if 'captcha' in await page.content():
-                await self.captcha()
-
-            print("Sleep Over")
-
-            if 'special delivery' in await page.content():
-                print("Spl delivery option")
-                await page.click("//span[contains(@data-a-tooltip-button-blocker,'options-continue-button')]")
-
-
-
-            await page.wait_for_selector("//div[@aria-label='Other UPI Apps']")
-
-            await asyncio.sleep(await self.random_delay())
-
-            await page.click("//div[@aria-label='Other UPI Apps'][.//input[@type='radio']]")
-
-            await page.type("//input[@placeholder='Enter UPI ID']", 'ksjeevithakannan123@okicici', delay=1)
-
-            await page.click("//input[@name='ppw-widgetEvent:ValidateUpiIdEvent']")
-
-            await asyncio.sleep(await self.random_delay())
-
-            await page.click("//input[@name='ppw-widgetEvent:SetPaymentPlanSelectContinueEvent']")
-
-            await page.wait_for_timeout(7500)
-            if 'Prime' in await page.content():
-                await page.click("//*[@id='prime-interstitial-nothanks-button']")
-
-
-            await page.wait_for_selector("//span[@id='subtotals-marketplace-spp-bottom']")
-
-            prices = {}
             try:
-                items_price = await page.text_content(
-                    "//tr[.//td[contains(., 'Items')]]//td[contains(@class, 'a-text-right')]", timeout=1000)
-                delivery_price = await page.text_content(
-                    "//tr[.//td[contains(., 'Delivery')]]//td[contains(@class, 'a-text-right')]", timeout=1000)
-                total_price = await page.text_content(
-                    "//tr[.//td[contains(., 'Total')]]//td[contains(@class, 'a-text-right')]", timeout=1000)
-                promotion_price = await page.text_content(
-                    "//tr[.//td[contains(., 'Promotion')]]//td[contains(@class, 'a-text-right')]", timeout=1000)
-                order_total_price = await page.text_content(
-                    "//tr[.//td[contains(., 'Order')]]//td[contains(@class, 'a-text-right')]", timeout=1000)
-            except Exception:
-                pass
-            # adding prices to the dictionary
-            try:
-                prices['items'] = items_price.strip()
-                prices['delivery'] = delivery_price.strip()
-                prices['total'] = total_price.strip()
-                prices['promotion applied'] = promotion_price.strip()
-                prices['order total'] = order_total_price.strip()
-            except Exception:
-                pass
+                browser = await p.chromium.launch(headless=True)
+                context = await browser.new_context()
 
-            print(prices)
+                self.page = await context.new_page()
+                page = self.page
+                await stealth_async(page)
+
+                await page.goto(url, wait_until='domcontentloaded')
+                await page.click("//input[@id='buy-now-button']")
+
+                await page.fill("//input[@type='email']", '9894789409')
+
+                await page.click("//input[@id='continue']")
+
+                await page.fill("//input[@type='password']", 'jeeva2005')
+
+                await page.click("//input[@id='signInSubmit']")
+                await page.wait_for_load_state("domcontentloaded")
+                await page.wait_for_timeout(2000)
+
+                if 'captcha' in await page.content():
+                    await self.captcha()
+
+                if 'Choose special delivery options' in await page.content():
+                    print("Spl delivery option")
+                    await page.click("//span[contains(@data-a-tooltip-button-blocker,'options-continue-button')]")
+
+                # await page.wait_for_selector("//input[@type='password']")
+                #
+                # await page.fill("//input[@type='password']", '123')
+                # await page.set_checked("//input[@type='checkbox']", True)
+
+                await page.wait_for_selector("//div[@aria-label='Other UPI Apps']")
+
+                await page.click("//div[@aria-label='Other UPI Apps'][.//input[@type='radio']]")
+
+                await page.fill("//input[@placeholder='Enter UPI ID']", 'ksjeevithakannan123@okicici')
+
+                await page.click("//input[@name='ppw-widgetEvent:ValidateUpiIdEvent']")
+
+                await page.click("//input[@name='ppw-widgetEvent:SetPaymentPlanSelectContinueEvent']", delay=5)
+
+                try:
+                    await page.wait_for_selector("//*[@id='prime-interstitial-nothanks-button']", state='visible', timeout=8000)
+                    await page.wait_for_timeout(1000)
+                    await page.evaluate('document.querySelector("#prime-interstitial-nothanks-button").click()')
+
+                except Exception:
+                    pass
+                await page.wait_for_selector("//span[@id='subtotals-marketplace-spp-bottom']", timeout=45000)
+                await page.wait_for_timeout(3000)
+
+                data = {}
+                rows = await page.query_selector_all("//*[@id='subtotals-marketplace-table']/tbody/tr")
+                for row in rows:
+                    cells = await row.query_selector_all("td")
+
+                    if len(cells) == 2:
+                        key = await cells[0].inner_text()
+                        key.replace('\n', '', ).strip()
+
+                        value = await cells[1].inner_text()
+                        value.replace('\n', '').strip()
+
+                        data[key] = value
+
+                print(data)
+
+                await browser.close()
+
+                return data
+
+
+
+            # prices = {}
+
+            # input()
+            # try:
+            #     items_price = await page.text_content(
+            #         "//tr[.//td[contains(., 'Items')]]//td[contains(@class, 'a-text-right')]", timeout=1)
+            #     order_total_price = await page.text_content(
+            #         "//tr[.//td[contains(., 'Order')]]//td[contains(@class, 'a-text-right')]", timeout=1)
+            #     delivery_price = await page.text_content(
+            #         "//tr[.//td[contains(., 'Delivery')]]//td[contains(@class, 'a-text-right')]", timeout=1)
+            #     total_price = await page.text_content(
+            #         "//tr[.//td[contains(., 'Total')]]//td[contains(@class, 'a-text-right')]", timeout=1)
+            #     promotion_price = await page.text_content(
+            #         "//tr[.//td[contains(., 'Promotion')]]//td[contains(@class, 'a-text-right')]", timeout=1)
+            #
+            # except Error:
+            #     pass
+            # # adding prices to the dictionary
+            # try:
+            #     prices['items'] = items_price.strip()
+            #     prices['delivery'] = delivery_price.strip()
+            #     prices['total'] = total_price.strip()
+            #     prices['promotion applied'] = promotion_price.strip()
+            #     prices['order total'] = order_total_price.strip()
+            # except Exception:
+            #     pass
+            #
+            # print(prices)
 
             # Close the browser
-            await browser.close()
 
-            return prices
+
+            except Exception as e:
+                print("Error")
+                url = page.url.strip()
+                html = await page.content()
+                with open('error.html', 'w', encoding='UTF-8') as f:
+                    f.write(html)
+                with open('error.txt', 'a') as f1:
+                    f1.write('\n' + str(e) + '\n')
+                    f1.write(url)
+
+    def __del__(self):
+        try:
+            os.remove(f'captcha_{self.uuidn}.txt')
+            os.remove(f'captcha_{self.uuidn}.png')
+        except FileNotFoundError:
+            pass
--- quart_app.py
+++ quart_app.py
@@ -1,9 +1,11 @@
 from quart import Quart, jsonify, request, send_file
 from app import Amazon
-import asyncio
 
 app = Quart(__name__)
-amazon_instance = Amazon()
+
+global uuidn
+global captcha_count
+global amazon_instance
 
 @app.route("/amazon", methods=['POST'])
 async def login_route():
@@ -12,21 +14,45 @@ async def login_route():
     requester_ip = request.remote_addr
     print(f"Requester IP Address: {requester_ip}")
     print(f"Requester data: {request_data}")
-    if 'url' in request_data:
-        url = request_data['url']
-        result = await amazon_instance.run(url)
-        return jsonify(result), 200
-    else:
-        return jsonify({'error': "Enter valid url"}), 400
+    try:
+        if 'url' in request_data:
+            url = request_data['url']
+            if '/dp/' in url:
+                global uuidn
+                global captcha_count
+                global amazon_instance
+
+                amazon_instance = Amazon()
+                uuidn = amazon_instance.uuidn
+                captcha_count = amazon_instance.captcha_count
+
+                result = await amazon_instance.run(url)
+                return jsonify(result), 200
+            else:
+                return jsonify({'error': "Enter full product url. Short url not accepted"}), 400
+        else:
+            return jsonify({'error': "Enter valid url"}), 400
+    except TypeError:
+        return jsonify({'error': "Enter url"}), 400
 
 
 @app.route('/captcha', methods=['GET'])
 async def get_captcha():
     requester_ip = request.remote_addr
     print(f"Requester IP Address: {requester_ip}")
-    captcha_image_path = f"captcha.png"
 
-    return await send_file(captcha_image_path, mimetype='image/png'), 300
+    try:
+        with open(f'captcha_{login_route}.txt', 'r') as f:
+            cou = f.read()
+            print(cou)
+            print(captcha_count)
+            if captcha_count == int(cou):
+                return await send_file(f'captcha_{uuidn}.png', mimetype='image/png'), 200
+            else:
+                return jsonify({"msg": "try_again"}), 401
+
+    except FileNotFoundError:
+        return jsonify({"msg": "No captcha"}), 200
 
 
 @app.route('/solve', methods=['POST'])

657c87c48698a7745d9cc5a4ab6ed2e19648268e
jeevithakannan2 <ksjeevithakannan123@gmail.com>
2024-02-12 10:16:56 +0530

    deleted:    after captcha.png 	deleted:    before captcha.png 	renamed:    flask_app.py -> quart_app.py 	modified:   app.py 	modified:   quart_app.py 	modified:   requirements.txt

--- quart_app.py
+++ quart_app.py
@@ -1,39 +1,37 @@
-from flask import Flask, jsonify, request, send_file
+from quart import Quart, jsonify, request, send_file
 from app import Amazon
 import asyncio
 
-app = Flask(__name__)
+app = Quart(__name__)
 amazon_instance = Amazon()
-#
-loop = asyncio.new_event_loop()
-
 
 @app.route("/amazon", methods=['POST'])
-def login_route():
-    # Assuming you receive some data in the request, for example, JSON data
-    request_data = request.get_json()
+async def login_route():
+
+    request_data = await request.get_json()
     requester_ip = request.remote_addr
     print(f"Requester IP Address: {requester_ip}")
     print(f"Requester data: {request_data}")
     if 'url' in request_data:
         url = request_data['url']
-        return jsonify(loop.run_until_complete(amazon_instance.run(url))), 200
+        result = await amazon_instance.run(url)
+        return jsonify(result), 200
     else:
         return jsonify({'error': "Enter valid url"}), 400
 
 
 @app.route('/captcha', methods=['GET'])
-def get_captcha():
+async def get_captcha():
     requester_ip = request.remote_addr
     print(f"Requester IP Address: {requester_ip}")
     captcha_image_path = f"captcha.png"
 
-    return send_file(captcha_image_path, mimetype='image/png'), 300
+    return await send_file(captcha_image_path, mimetype='image/png'), 300
 
 
 @app.route('/solve', methods=['POST'])
-def solve_captcha():
-    request_data = request.get_json()
+async def solve_captcha():
+    request_data = await request.get_json()
 
     if 'captcha' in request_data:
 
@@ -41,7 +39,7 @@ def solve_captcha():
         requester_ip = request.remote_addr
         print(f"Requester IP Address: {requester_ip}")
         print(f"Requester data: {request_data}")
-        result = loop.create_task(amazon_instance.captcha_inp(captcha))  # Start the captcha-solving thread
+        result = await amazon_instance.captcha_inp(captcha)
         return jsonify(result), 201
     else:
         return jsonify({'error': 'Missing captcha or url'}), 401
